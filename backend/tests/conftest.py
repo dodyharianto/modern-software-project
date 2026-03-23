a7656b0c017ca9a794e49f158b1509b2eecf221f
alohchab-cpu <akash.l.2024@mitb.smu.edu.sg>
2026-03-23 21:15:19 +0800

    Reorganize import statements and path setup

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,9 +2,9 @@ import sys
 import os
 from pathlib import Path
 
-#tests_dir = Path(__file__).resolve().parent
-#backend_dir = tests_dir.parent
-#project_dir = backend_dir.parent
+tests_dir = Path(__file__).resolve().parent
+backend_dir = tests_dir.parent
+project_dir = backend_dir.parent
 
 sys.path.insert(0, str(project_dir))
 sys.path.insert(0, str(backend_dir))
